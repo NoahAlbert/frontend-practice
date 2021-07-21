607f407f2bb9157022499633702bbf0597e2ae0b
NoahAlbert <no.albert113@gmail.com>
2021-07-21 13:27:57 -0400

    modified submission form, connected to api, and linked form to information shown

--- bc-web-html/scripts.js
+++ bc-web-html/scripts.js
@@ -2,16 +2,42 @@ function handleSubmit(event) {
     event.preventDefault();
     console.log("Submitted Form");
     const form = document.querySelector("form");
-    form.innerHTML = "<h3>Submitted</h3>";
+    const newCustomer = {
+      firstName: form.elements.firstName.value,
+      lastName: form.elements.lastName.value,
+      email: form.elements.email.value,
+    }    
+    console.log(newCustomer)
+    fetch('https://na-pet-boutique.web.app/customers', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newCustomer)
+    })
+    .then(() => {
+      form.innerHTML = '<h3>Submitted</h3>'
+      getCustomers()
+    })
+    .catch(err => form.innerHTML = '<h3>Error Sending Customer</h3>')
   }
 
-function handleNameChange() {
-     console.log('name changed', event.target.value)
+
+function getCustomers(){
+  fetch('https://na-pet-boutique.web.app/customers')
+  .then(res => res.json())
+  .then(data => {
+    const custDiv = document.getElementById('customers')
+    custDiv.innerHTML = ''
+    const custArray = data.map(customer => {
+        return `<article>
+        <h3>${customer.firstName} ${customer.lastName}</h3>
+        <p>${customer.email}</p>
+        </article>`
+      })
+    custDiv.innerHTML = custArray.join('')
+    })
+  .catch(err => console.log(err))
 }
 
-// const hero = document.getElementById("hero");
-// hero.innerText = "This is my new Hero Title";
-// const links = document.getElementsByClassName("menu-link");
-// links[1].style.color = "white";
-// const footer = document.querySelector("footer");
-// footer.style.backgroundColor = "grey";
+getCustomers()
